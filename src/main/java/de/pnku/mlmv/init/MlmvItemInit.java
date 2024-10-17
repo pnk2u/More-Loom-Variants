464b9b8a68a4ad2bd15252038a118a0e5e2e46a5
pnk2u <159317511+pnk2u@users.noreply.github.com>
2024-10-17 08:26:20 +0200

    Initial Commit (1.21.2)

--- src/main/java/de/pnku/mlmv/init/MlmvItemInit.java
+++ src/main/java/de/pnku/mlmv/init/MlmvItemInit.java
@@ -4,24 +4,36 @@ import de.pnku.mlmv.MoreLoomVariants;
 import de.pnku.mlmv.block.MoreLoomVariantBlock;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraft.core.Registry;
 
-public class MlmvItemInit {
-    public static final BlockItem BIRCH_LOOM_I = new BlockItem(MlmvBlockInit.BIRCH_LOOM, new Item.Properties());
-    public static final BlockItem DARK_OAK_LOOM_I = new BlockItem(MlmvBlockInit.DARK_OAK_LOOM, new Item.Properties());
-    public static final BlockItem SPRUCE_LOOM_I = new BlockItem(MlmvBlockInit.SPRUCE_LOOM, new Item.Properties());
-    public static final BlockItem JUNGLE_LOOM_I = new BlockItem(MlmvBlockInit.JUNGLE_LOOM, new Item.Properties());
-    public static final BlockItem ACACIA_LOOM_I = new BlockItem(MlmvBlockInit.ACACIA_LOOM, new Item.Properties());
-    public static final BlockItem MANGROVE_LOOM_I = new BlockItem(MlmvBlockInit.MANGROVE_LOOM, new Item.Properties());
-    public static final BlockItem CHERRY_LOOM_I = new BlockItem(MlmvBlockInit.CHERRY_LOOM, new Item.Properties());
-    public static final BlockItem BAMBOO_LOOM_I = new BlockItem(MlmvBlockInit.BAMBOO_LOOM, new Item.Properties());
-    public static final BlockItem CRIMSON_LOOM_I = new BlockItem(MlmvBlockInit.CRIMSON_LOOM, new Item.Properties());
-    public static final BlockItem WARPED_LOOM_I = new BlockItem(MlmvBlockInit.WARPED_LOOM, new Item.Properties());
+import static de.pnku.mlmv.init.MlmvBlockInit.*;
 
+public class MlmvItemInit {
+    public static final BlockItem BIRCH_LOOM_I = itemFromBlock(BIRCH_LOOM);
+    public static final BlockItem DARK_OAK_LOOM_I = itemFromBlock(DARK_OAK_LOOM);
+    public static final BlockItem SPRUCE_LOOM_I = itemFromBlock(SPRUCE_LOOM);
+    public static final BlockItem JUNGLE_LOOM_I = itemFromBlock(JUNGLE_LOOM);
+    public static final BlockItem ACACIA_LOOM_I = itemFromBlock(ACACIA_LOOM);
+    public static final BlockItem MANGROVE_LOOM_I = itemFromBlock(MANGROVE_LOOM);
+    public static final BlockItem CHERRY_LOOM_I = itemFromBlock(CHERRY_LOOM);
+    public static final BlockItem BAMBOO_LOOM_I = itemFromBlock(BAMBOO_LOOM);
+    public static final BlockItem CRIMSON_LOOM_I = itemFromBlock(CRIMSON_LOOM);
+    public static final BlockItem WARPED_LOOM_I = itemFromBlock(WARPED_LOOM);
+
+    public static BlockItem itemFromBlock(MoreLoomVariantBlock moreLoomVariantBlock) {
+        return new BlockItem(moreLoomVariantBlock, setProperties(moreLoomVariantBlock));
+    }
+
+    public static Item.Properties setProperties(MoreLoomVariantBlock moreLoomVariantBlock) {
+        return new Item.Properties()
+                .setId(ResourceKey.create(Registries.ITEM,BuiltInRegistries.BLOCK.getKey(moreLoomVariantBlock))).useBlockDescriptionPrefix();
+    }
 
     public static void registerItems() {
         registerItem(BIRCH_LOOM_I, Items.LOOM);
